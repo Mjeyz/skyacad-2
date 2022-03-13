4b609e849846134244cbbbfeb7c09ffe31333f63
MJeyz <thisismjey@gmail.com>
2022-03-14 00:33:28 +0500

    updated the footer section stylings

--- resources/lang/en/footer.php
+++ resources/lang/en/footer.php
@@ -11,6 +11,6 @@ return [
     'join' => 'Join',
     'join_us_today' => 'Join us today',
     'subscribe_content' => '#We will send the best deals and offers to your email.',
-    'enter_email_here' => 'Enter your email here',
+    'enter_email_here' => 'Email Address',
 
 ];
